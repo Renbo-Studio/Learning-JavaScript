f6d9b109435a856d103faef4fedb93e9e5db7ad6
Joshua Umahi <98918202+Renbo-Studio@users.noreply.github.com>
2022-12-28 15:35:08 +0100

    Update index.js
    Lol

--- index.js
+++ index.js
@@ -257,30 +257,7 @@ let bot2 = document.getElementById("bot2")
 let bot1 = document.getElementById("bot1")
 let botr = document.getElementById("botR")
 
-/* Get the documentElement (<html>) to display the page in fullscreen */
-var elem = document.documentElement;
 
-/* View in fullscreen */
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
-/* Close fullscreen */
-function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) { /* Safari */
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE11 */
-    document.msExitFullscreen();
-  }
-}
 
 console.log(createdQuestion)
 
